1dbf09ec0eca67e3c3c8c3fc3946e76ef6b80698
vCarbonnn <carbonontagpro@gmail.com>
2016-10-24 21:44:53 +0100

    Create UserSearchEnhancement.user.js

---
+++ UserSearchEnhancement.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Mod Tools User Search Enhancement
+// @author	 	 Carbon
+// @version      1.0
+// @description  Pressing enter when searching for a user will change the played within filter to whenever and filter for you in TagPro Mod Tools.
+// @updateURL    https://github.com/vCarbonnn/UserSearchEnhancement/raw/master/UserSearchEnhancement.user.js
+// @include      http://tagpro-*.koalabeast.com/moderate/users*
+// ==/UserScript==
+
+document.getElementById("filterName").onkeypress = function(e) {
+    var event = e || window.event;
+    var charCode = event.which || event.keyCode;
+
+    if ( charCode == '13' ) {
+        document.getElementById("filterHours").value = "whenever";
+        document.getElementById("filterButton").click();
+    }
+};
